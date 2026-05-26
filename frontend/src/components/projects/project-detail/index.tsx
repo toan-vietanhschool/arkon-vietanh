9383d54bb63a607ca9ab3d58c8dc5a22e18186cf
toan <toan.nguyencong@truongvietanh.com>
2026-05-26 17:24:32 +0700

    Revert "feat(workspace-wiki): pin global wiki pages into a workspace"
    This reverts commit 35b797f662187c70257efa06c2efeaf2ab86bdd8.

--- frontend/src/components/projects/project-detail/index.tsx
+++ frontend/src/components/projects/project-detail/index.tsx
@@ -220,8 +220,6 @@ export function ProjectDetail({ project, isAdmin, onBack }: Props) {
           wikiPages={wikiPages}
           wikiLoading={wikiLoading}
           wikiIndexMd={wikiIndexMd}
-          onWikiChanged={loadWiki}
-          canEdit={canEditWorkspace}
         />
       )}
     </div>
